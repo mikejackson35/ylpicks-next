3a51a6fe8392cea7dac0ff6caf4e74b9532b2d36
Mike_Jackson <mikejackson35@gmail.com>
2026-02-25 21:06:19 -0800

    Center main content area with max-w-4xl on desktop

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,9 @@ export default async function RootLayout({
           {session ? (
             <div className="md:flex min-h-screen">
               <Sidebar standings={standings} thruText={thruText} />
-              <main className="flex-1 p-4 md:p-10 overflow-auto pb-20 md:pb-10">{children}</main>
+              <main className="flex-1 overflow-auto pb-20 md:pb-10">
+                <div className="max-w-4xl mx-auto p-4 md:p-10">{children}</div>
+              </main>
             </div>
           ) : (
             <main className="flex-1">{children}</main>
